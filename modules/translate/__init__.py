83e7eeff8607115132638760d30625f6aa4e53c2
Dianliang233 <dianliang233@gmail.com>
2023-09-08 07:56:12 +0800

    We don't need to time everything

--- modules/translate/__init__.py
+++ modules/translate/__init__.py
@@ -3,13 +3,13 @@ from core.builtins import Bot, Plain
 from core.component import module
 from core.utils.http import get_url
 from config import Config
-import time
-import hashlib, random
+import hashlib
+import random
 
 trans = module(bind_prefix='translate', desc='{trans.desc}', alias=['trans'], developers=['bugungu'])
 
-@trans.handle('<from> <to> <query> {{trans.desc.trans}}')
 
+@trans.handle('<from> <to> <query> {{trans.desc.trans}}')
 async def send(msg: Bot.MessageSession):
     fm = msg.parsed_msg['<from>']
     to = msg.parsed_msg['<to>']
@@ -19,19 +19,14 @@ async def send(msg: Bot.MessageSession):
     key = Config('translate_api_key')
     sign = hashlib.md5((appid + q + salt + key).encode('utf-8'))
     Logger.info(sign.hexdigest() + ' ' + appid + q + salt + key)
-    start = time.perf_counter_ns()
     url = 'http://api.fanyi.baidu.com/api/trans/vip/translate?q=' + q + '&from=' + fm + '&to=' + to + \
-            '&appid=' + appid + '&salt=' + salt + '&sign=' + sign.hexdigest() + '&action=1'
+        '&appid=' + appid + '&salt=' + salt + '&sign=' + sign.hexdigest() + '&action=1'
     res = await get_url(url, fmt='json', timeout=200)
     Logger.info('Got result: ' + str(res))
     if 'error_code' in res.keys():
-        await msg.finish(msg.locale.t('trans.err.prefix') + msg.locale.t('trans.err.'+str(res['error_code'])))
+        await msg.finish(msg.locale.t('trans.err.prefix') + msg.locale.t('trans.err.' + str(res['error_code'])))
     send_msg = []
     send_msg.append(Plain(res['trans_result'][0]['dst']))
     if res['trans_result'][0]['dst'] == '':
         send_msg.append(Plain(msg.locale.t('trans.msg.none')))
-    stop = time.perf_counter_ns()
-    delta = (stop - start) / 1000000
-    if msg.checkSuperUser():
-        send_msg.append(Plain(msg.locale.t('trans.msg.time', time=delta)))
     await msg.finish(send_msg)
