269245bc2d50911e53ae033d664f643b3200e97e
Silian Zheng <SilianZ>
2023-09-10 22:21:15 +0800

    Update the i18n keys and options descriptions.
    But there still have bugs lol.

--- modules/translate/__init__.py
+++ modules/translate/__init__.py
@@ -1,5 +1,4 @@
-from core.logger import Logger
-from core.builtins import Bot, Plain
+from core.builtins import Bot
 from core.component import module
 from core.utils.http import get_url
 from config import Config
@@ -7,18 +6,19 @@ import hashlib
 import random
 
 trans = module(bind_prefix='translate', desc='{trans.desc}', alias=['trans'], developers=['bugungu'])
-appid = Config('translate_appid')
 
+@trans.handle('<from> <to> <query> {{trans.help}}', 
+              options_desc={'<from>': '{trans.help.opt.from}', '<to>': '{trans.help.opt.to}', '<query>': '{trans.help.opt.query}'})
 
-@trans.handle('<from> <to> <query> {{trans.desc.trans}}')
 async def send(msg: Bot.MessageSession):
     fm = msg.parsed_msg['<from>']
     to = msg.parsed_msg['<to>']
+    appid = Config('translate_appid')
     q = msg.parsed_msg['<query>']
-
     salt = str(random.randint(32768, 65536))
     key = Config('translate_api_key')
     sign = hashlib.md5((appid + q + salt + key).encode('utf-8'))
+
     url = 'http://api.fanyi.baidu.com/api/trans/vip/translate?q=' + q + '&from=' + fm + '&to=' + to + \
         '&appid=' + appid + '&salt=' + salt + '&sign=' + sign.hexdigest() + '&action=1'
     res = await get_url(url, fmt='json', timeout=200)
@@ -27,6 +27,6 @@ async def send(msg: Bot.MessageSession):
         raise ValueError("{trans.err.prefix}{trans.err." + str(res['error_code']) + "}")
     send_msg = res['trans_result'][0]['dst']
 
-    if res['trans_result'][0]['dst'] == '':
+    if send_msg == '':
         send_msg += '\n' + '{trans.msg.none}'
     await msg.finish(send_msg)
