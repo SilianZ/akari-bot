5ddb60c4a9dd1b8b6bde356ea144b3db2d1d7810
Dianliang233 <dianliang233@gmail.com>
2023-09-10 08:32:39 +0800

    We don't need to log everything as well

--- modules/translate/__init__.py
+++ modules/translate/__init__.py
@@ -7,6 +7,7 @@ import hashlib
 import random
 
 trans = module(bind_prefix='translate', desc='{trans.desc}', alias=['trans'], developers=['bugungu'])
+appid = Config('translate_appid')
 
 
 @trans.handle('<from> <to> <query> {{trans.desc.trans}}')
@@ -14,19 +15,18 @@ async def send(msg: Bot.MessageSession):
     fm = msg.parsed_msg['<from>']
     to = msg.parsed_msg['<to>']
     q = msg.parsed_msg['<query>']
-    appid = Config('translate_appid')
+
     salt = str(random.randint(32768, 65536))
     key = Config('translate_api_key')
     sign = hashlib.md5((appid + q + salt + key).encode('utf-8'))
-    Logger.info(sign.hexdigest() + ' ' + appid + q + salt + key)
     url = 'http://api.fanyi.baidu.com/api/trans/vip/translate?q=' + q + '&from=' + fm + '&to=' + to + \
         '&appid=' + appid + '&salt=' + salt + '&sign=' + sign.hexdigest() + '&action=1'
     res = await get_url(url, fmt='json', timeout=200)
-    Logger.info('Got result: ' + str(res))
+
     if 'error_code' in res.keys():
-        await msg.finish(msg.locale.t('trans.err.prefix') + msg.locale.t('trans.err.' + str(res['error_code'])))
-    send_msg = []
-    send_msg.append(Plain(res['trans_result'][0]['dst']))
+        await msg.finish("{trans.err.prefix}{trans.err." + str(res['error_code']) + "}")
+    send_msg = res['trans_result'][0]['dst']
+
     if res['trans_result'][0]['dst'] == '':
-        send_msg.append(Plain(msg.locale.t('trans.msg.none')))
+        send_msg += '\n' + '{trans.msg.none}'
     await msg.finish(send_msg)
